239b228522aa4c3485134948acb093db52a87c9e
shreyash26cho <shreyashchougule26@gmail.com>
2025-02-24 03:00:28 -0800

    Add files via upload

---
+++ object sliceing.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Base{
+	int x;
+	public:
+		void show()
+	{
+		cout<<x<<endl;
+	}
+	
+};	
+	
+class D:public Base{
+	int y;
+	public:
+		void show()
+		{
+			cout<<x<<endl;
+			cout<
+		}
+		
+	
+};
+int main()
+{
+   base b1;
+   b1.show(4);	
+}
+
+
